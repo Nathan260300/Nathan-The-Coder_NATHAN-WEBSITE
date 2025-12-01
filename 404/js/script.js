128d6f60c88f18bab548855407ef40b32a9f40bd
Nathan <nathan162318@gmail.com>
2025-12-01 19:23:27 +0100

    Add files via upload

--- 404/js/script.js
+++ 404/js/script.js
@@ -1,20 +1,19 @@
 /*!
- * Portfolio Nathan - Page Accueil
+ * Portfolio Nathan - Page 404
  * Copyright (C) 2025 Nathan
  * Licensed under the GNU General Public License v3.0
  * You should have received a copy of the GNU GPL along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
 console.log("%c© 2025 - Nathan The Coder", "background: #282c34; color: #98c379; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
-console.log("%cAccueil - Nathan The Coder", "background: #282c34; color: #61afef; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
-console.log("%cPortfolio de Nathan, collégien et développeur passionné. Découvrez mes projets web, mon blog et mes compétences !","background: #282c34; color: #61dafb; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
-console.log("%chttps://nathan-the-coder.netlify.app", "background: #282c34; color: #e06c75; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%c404 - Nathan The Coder", "background: #282c34; color: #61afef; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%c404 Not Found : Tu t'es trompé de page !","background: #282c34; color: #61dafb; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%chttps://nathan-the-coder.netlify.app/?", "background: #282c34; color: #e06c75; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
 
 async function afficherDerniereMaj() {
   try {
     const res = await fetch("/.netlify/functions/get-last-update");
     const data = await res.json();
-
     const el = document.getElementById("last-update");
 
     if (!data.dernier_maj) {
@@ -39,4 +38,18 @@ async function afficherDerniereMaj() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", afficherDerniereMaj);
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.floating-menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('open');
+
+  // Si le menu vient de s'ouvrir, lancer le timer pour le fermer
+  if (menu.classList.contains('open')) {
+    setTimeout(() => {
+      menu.classList.remove('open');
+    }, 5000); // 5000ms = 5 secondes
+  }
+});
+
+document.addEventListener("DOMContentLoaded", afficherDerniereMaj)
