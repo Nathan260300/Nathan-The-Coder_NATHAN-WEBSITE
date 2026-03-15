5c7f5b871f7cb44085241d5b27c241a0a4798a75
Nathan Jacinto <njacinto@MBPdeFrancoise.lan>
2026-03-15 19:44:52 +0100

    MAJ

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log(`%c© 2026 - Nathan The Coder`, "background: #282c34; color: #98c379; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
 console.log("%cNathan The Coder", "background: #282c34; color: #61afef; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
-console.log("%cDashboard Web de modération. Connexion réservée au staff.","background: #282c34; color: #61dafb; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cPortfolio de Nathan — Développeur web & bot Discord passionné.","background: #282c34; color: #61dafb; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
 console.log("%chttps://nathan-the-coder.netlify.app", "background: #282c34; color: #e06c75; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
 console.log(`%cMade with 🕑 and 💖 by Nathan`,"background: #282c34; color: #c678dd; padding: .5em 1em; border-radius: 5px; font-weight: bold;")
 
